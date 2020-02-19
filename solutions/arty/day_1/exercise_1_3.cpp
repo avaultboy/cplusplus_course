04d1d1c0e4c723f8e1d44601948ed91a3bb791bd
Artsiom-Yudziankou <57104024+Artsiom-Yudziankou@users.noreply.github.com>
2020-02-19 12:40:17 +0300

    Update exercise_1_3.cpp

--- solutions/arty/day_1/exercise_1_3.cpp
+++ solutions/arty/day_1/exercise_1_3.cpp
@@ -21,42 +21,67 @@ int main()
 	cout
 		<< "Enter left operand :" << endl;
 	cin >> operand_1;
+
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Invalid Operand, try one more time:" << endl;
+		cin >> operand_1;
+	}
+
 	cout << "Enter operator to apply" << endl;
 	cin >> operator_to_use;
 	cout << "Enter right operand :" << endl;
+
 	cin >> operand_2;
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Invalid Operand, try one more time:" << endl;
+		cin >> operand_2;
+	}
+
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Invalid" << endl;
+	}
 
 	if (operator_to_use == '+')
 	{
-		cout << (operand_1 + operand_2) << endl;
+		cout << "Result:"
+			 << (operand_1 + operand_2) << endl;
 	}
 	else if (operator_to_use == '-')
 	{
-		cout << (operand_1 - operand_2) << endl;
+		cout << "Result:" << (operand_1 - operand_2) << endl;
 	}
 	else if (operator_to_use == '/')
 	{
-		cout << (operand_1 / operand_2) << endl;
+		cout << "Result:" << (operand_1 / operand_2) << endl;
 	}
 	else if (operator_to_use == '*')
 	{
-		cout << (operand_1 * operand_2) << endl;
+		cout << "Result:" << (operand_1 * operand_2) << endl;
 	}
 	else if (operator_to_use == '%')
 	{
-		cout << ((int)operand_1 % (int)operand_2) << endl;
+		cout << "Result:" << ((int)operand_1 % (int)operand_2) << endl;
 	}
 	else if (operator_to_use == '|')
 	{
-		cout << ((int)operand_1 | (int)operand_2) << endl;
+		cout << "Result:" << ((int)operand_1 | (int)operand_2) << endl;
 	}
 	else if (operator_to_use == '&')
 	{
-		cout << ((int)operand_1 & (int)operand_2) << endl;
+		cout << "Result:" << ((int)operand_1 & (int)operand_2) << endl;
 	}
 	else if (operator_to_use == '^')
 	{
-		cout << ((int)operand_1 ^ (int)operand_2) << endl;
+		cout << "Result:" << ((int)operand_1 ^ (int)operand_2) << endl;
 	}
 	else
 	{
